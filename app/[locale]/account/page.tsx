70c02cab33a8e39cf51c993192678e6966a21359
Fe-56 <limfuoen1999@gmail.com>
2024-08-17 22:11:57 +0800

    Set up account page to demonstrate that authentication works
    Well, at least for Google

--- app/[locale]/account/page.tsx
+++ app/[locale]/account/page.tsx
@@ -1,7 +1,24 @@
+"use client"
+
+import { useSession } from "next-auth/react";
+import { useTranslations } from "next-intl";
 import React from "react";
 
 const Account = () => {
-  return <h1>Account</h1>;
+  const t = useTranslations();
+  const {data: session} = useSession();
+  const name: string = session?.user?.name!;
+  const email: string = session?.user?.email!;
+
+  console.log(`name: ${name}`);
+
+  return (
+    <div>
+      <h1>Account</h1>
+      <h2>{t("account.welcome", {name: name})}</h2>
+      <h2>{`Email: ${email}`}</h2>
+    </div>
+  );
 };
 
 export default Account;
