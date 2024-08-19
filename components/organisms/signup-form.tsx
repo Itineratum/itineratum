4e634e3b52e4ac1eb664754365306eb4db7c0e61
Fe-56 <limfuoen1999@gmail.com>
2024-08-19 23:39:16 +0800

    Add validation to the phone number field in the login page
    Need to see how to separate the country calling code and the phone number inputs

--- components/organisms/signup-form.tsx
+++ components/organisms/signup-form.tsx
@@ -4,10 +4,16 @@ import Text from "@/components/atoms/text";
 import { TypographyVariant } from "@/constants/enums/theme";
 import colorsConst from "@/constants/pages/colors.json";
 import urlConst from "@/constants/urls.json";
-import { Box, Button, InputLabel, MenuItem, TextField } from "@mui/material";
+import { Box, Button, MenuItem, TextField } from "@mui/material";
 import { getCountryDataList } from "countries-list";
+import {
+  CountryCode,
+  getCountryCallingCode,
+  isValidPhoneNumber,
+} from "libphonenumber-js";
 import { useTranslations } from "next-intl";
 import Link from "next/link";
+import { useState } from "react";
 
 const SignUpForm = () => {
   const t = useTranslations();
@@ -16,59 +22,120 @@ const SignUpForm = () => {
   const countries = getCountryDataList();
   const color = colorsConst.components.textField;
   const formMargin: number = 2;
+  const formFieldMargin: "dense" | "normal" | "none" | undefined = "normal";
+  const formFieldBorderRadius: number = 2;
+  const formFieldStyling: Object = {
+    backgroundColor: color.backgroundColor,
+    borderRadius: formFieldBorderRadius,
+    "& .MuiFilledInput-root": {
+      borderRadius: formFieldBorderRadius, // ensure the filled area is rounded
+      "&:before, &:after": {
+        borderBottom: "none", // remove the underline when the text field is being clicked on
+      },
+    },
+    "& .MuiInputBase-input": {
+      borderRadius: formFieldBorderRadius, // ensure the input text area is rounded
+    },
+  };
+  const [country, setCountry] = useState<string>("");
+  const [phoneNumber, setPhoneNumber] = useState<string>("");
 
   // TODO
   const handleSubmit = () => {};
 
   const countryField = () => {
+    const dropdownHeight: number = 200;
+
+    const handleCountryChange = (
+      event: React.ChangeEvent<HTMLInputElement>,
+    ) => {
+      const country = event.target.value;
+      setCountry(country);
+
+      try {
+        const callingCode = getCountryCallingCode(country as CountryCode);
+        setPhoneNumber(`+${callingCode} `);
+      } catch (error) {
+        setPhoneNumber("");
+      }
+    };
+
     return (
-      <Box marginTop={formMargin} sx={{ width: "100%" }}>
-        <InputLabel id={countryFieldId} sx={{ color: "text.primary" }}>
-          {t("login.loginForm.country")}
-        </InputLabel>
-        <TextField
-          select
-          required
-          fullWidth
-          id={countryFieldId}
-          sx={{
-            backgroundColor: color.backgroundColor,
-          }}
-          SelectProps={{
-            MenuProps: {
-              MenuListProps: {
-                sx: { maxHeight: 200, overflowY: "auto" },
-              },
+      <TextField
+        select
+        required
+        fullWidth
+        id={countryFieldId}
+        variant="filled"
+        sx={formFieldStyling}
+        margin={formFieldMargin}
+        label={t("login.loginForm.country")}
+        value={country}
+        onChange={handleCountryChange}
+        InputLabelProps={{ sx: { color: "text.primary" } }}
+        SelectProps={{
+          MenuProps: {
+            MenuListProps: {
+              sx: { maxHeight: dropdownHeight, overflowY: "auto" },
             },
-          }}
-        >
-          {countries.map((country) => (
-            <MenuItem key={country.name} value={country.name}>
-              {country.name}
-            </MenuItem>
-          ))}
-        </TextField>
-      </Box>
+          },
+        }}
+      >
+        {countries.map((country) => (
+          <MenuItem key={country.name} value={country.iso2}>
+            {country.name}
+          </MenuItem>
+        ))}
+      </TextField>
     );
   };
   const numberField = () => {
+    const [error, setError] = useState<boolean>(false);
+    const [helperText, setHelperText] = useState<string>("");
+
+    const handlePhoneNumberChange = (
+      event: React.ChangeEvent<HTMLInputElement>,
+    ) => {
+      const inputPhoneNumber = event.target.value;
+      setPhoneNumber(inputPhoneNumber);
+
+      const onError = () => {
+        setError(true);
+        setHelperText(t("login.loginForm.numberError"));
+      };
+
+      try {
+        if (!isValidPhoneNumber(inputPhoneNumber, country as CountryCode)) {
+          onError();
+        } else {
+          setError(false);
+          setHelperText("");
+        }
+      } catch (error) {
+        onError();
+      }
+    };
+
     return (
-      <Box marginTop={formMargin} sx={{ width: "100%" }}>
-        <InputLabel id={numberFieldId} sx={{ color: "text.primary" }}>
-          {t("login.loginForm.number")}
-        </InputLabel>
-        <TextField
-          required
-          fullWidth
-          id={numberFieldId}
-          sx={{ backgroundColor: color.backgroundColor }}
-        />
-      </Box>
+      <TextField
+        required
+        fullWidth
+        id={numberFieldId}
+        variant="filled"
+        sx={formFieldStyling}
+        margin={formFieldMargin}
+        label={t("login.loginForm.number")}
+        value={phoneNumber}
+        InputLabelProps={{ sx: { color: "text.primary" } }}
+        onChange={handlePhoneNumberChange}
+        error={error}
+        helperText={helperText}
+      />
     );
   };
   const privacyPolicyLink = () => {
     return (
-      <Box marginTop={formMargin} sx={{ textAlign: "left", width: "100%" }}>
+      <Box sx={{ textAlign: "left", width: "100%" }}>
         <Link
           href={urlConst.privacyPolicy}
           color="text.primary"
@@ -84,12 +151,14 @@ const SignUpForm = () => {
     );
   };
   const signUpButton = () => {
+    const buttonWidth: string = "40%";
+
     return (
       <Button
         type="submit"
         fullWidth
         variant="contained"
-        sx={{ mt: formMargin, mb: formMargin, maxWidth: "40%" }}
+        sx={{ mt: formMargin, mb: formMargin, maxWidth: buttonWidth }}
       >
         <Text
           text={t("login.loginForm.signUp")}
