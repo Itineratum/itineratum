5167c456d8d00a32ec72d5993daa77fd3798aeff
Fe-56 <limfuoen1999@gmail.com>
2025-01-05 13:26:12 +0800

    Refactor Itinerary document on MongoDB
    Itineraries should be properly segmented day-by-day

--- services/database/itinerary.ts
+++ services/database/itinerary.ts
@@ -1,15 +1,33 @@
 import { connectToDatabase, disconnectFromDatabase } from "@/lib/db";
 import Itinerary from "@/models/Itinerary";
+import { getDays } from "@/utils/itinerary";
 
 export const saveItinerary = async (
   email: string | null,
   request: any,
-  itinerary: any,
+  itineraryRaw: any,
   hotels: any,
-  flights: any,
+  flights: any
 ) => {
   try {
     await connectToDatabase();
+    const itinerary: any[] = [];
+
+    itineraryRaw.map((locationItinerary: any) => {
+      const days = getDays(locationItinerary.day);
+      const location = locationItinerary.location;
+      locationItinerary.plan.map((rawDayPlan: any, index: number) => {
+        const dayPlan = {
+          location,
+          day: days[index],
+          morning: rawDayPlan.morning,
+          afternoon: rawDayPlan.afternoon,
+          evening: rawDayPlan.evening,
+        };
+        itinerary.push(dayPlan);
+      });
+    });
+
     const itineraryDocument = {
       generated_by: "",
       generated_at: new Date(),
@@ -32,5 +50,29 @@ export const saveItinerary = async (
     throw error;
   } finally {
     await disconnectFromDatabase();
+  }
+};
+
+export const retrieveItinerary = async (itineraryId: string) => {
+  try {
+    await connectToDatabase();
+    const itinerary = await Itinerary.findById(itineraryId);
+
+    if (itinerary) {
+      return {
+        success: true,
+        data: itinerary,
+      };
+    } else {
+      return {
+        success: false,
+        error: "Failed to retrieve Itinerary",
+      };
+    }
+  } catch (error) {
+    console.error(error);
+    throw error;
+  } finally {
+    // await disconnectFromDatabase();
   }
 };
---
+++ utils/itinerary.ts
@@ -0,0 +1,9 @@
+export const getDays = (dayRange: string): number[] => {
+  const dashIndex = dayRange.indexOf("-");
+  const startDay = Number(dayRange.slice(0, dashIndex));
+  const endDay = Number(dayRange.slice(dashIndex + 1));
+  return Array.from(
+    { length: endDay - startDay + 1 },
+    (_, i) => startDay + i
+  );
+};
