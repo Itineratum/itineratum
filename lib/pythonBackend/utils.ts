a5f79ce282294f5a0c29b6de813d69bdbd53f4c2
Fe-56 <limfuoen1999@gmail.com>
2025-01-15 09:53:26 +0800

    Refactoring of itinerary util function

--- lib/pythonBackend/utils.ts
+++ lib/pythonBackend/utils.ts
@@ -1,4 +1,3 @@
-import { getDays } from "@/utils/itinerary";
 import dayjs from "dayjs";
 import {
   OutputFormat,
@@ -137,6 +136,13 @@ export const getTravelTimes = async (
   }
 
   return itineraryTravelTimes;
+};
+
+export const getDays = (dayRange: string): number[] => {
+  const dashIndex = dayRange.indexOf("-");
+  const startDay = Number(dayRange.slice(0, dashIndex));
+  const endDay = Number(dayRange.slice(dashIndex + 1));
+  return Array.from({ length: endDay - startDay + 1 }, (_, i) => startDay + i);
 };
 
 export const formatItinerary = async (
